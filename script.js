eaca96b5736d7fc060fe40fda872ac35c6144af4
sangramjit barman <32595023+sangramjitb@users.noreply.github.com>
2025-02-21 10:06:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
 // Song titles
-const songs = [ 'just-a-dream',  'rox', 'wasted', 'sun', 'dj', '7', 'post', 'light', 'teen' , 'brian','die with a smile song', 'girls', 'lala', 'bye' ,'audio' ,
+const songs = [ 'just-a-dream',  'rox', 'sun',  '7', 'post', 'light', 'teen' , 'brian','die with a smile song', 'girls', 'lala', 'bye' ,'audio' ,
 	       'Set Fire to the Rain','Ava Max - Sweet but Psycho [Official Music Video]' , 'Maroon 5 - Animals (Lyrics)',
 	        'payphone', 'heat wave', 'all we know', 'The Neighbourhood - Sweater Weather', 'Sia - Cheap Thrills',
 	        'Ruth B. - Dandelions'  , 'closer', 'on-my-own', 'rockabye', 'starving'  ];
