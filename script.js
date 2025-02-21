f3cc30f5a293c901f63e9781a6a138c5c724e8cd
sangramjit barman <32595023+sangramjitb@users.noreply.github.com>
2025-02-21 10:18:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,11 @@ const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
 // Song titles
-const songs = [ 'just-a-dream',  'rox', 'sun',  '7', 'post', 'light', 'teen' , 'brian','die with a smile song', 'girls', 'lala', 'bye' ,'audio' ,
+const songs = [ 'gotus','night', 'cut', '2002', 'east', 'past' , 'trust', 'okay' , 'whatever'
+	       ,'rox', 'sun',  '7', 'post', 'light', 'teen' , 'brian','die with a smile song', 'girls', 'lala', 'bye' ,'audio' ,
 	       'Set Fire to the Rain','Ava Max - Sweet but Psycho [Official Music Video]' , 'Maroon 5 - Animals (Lyrics)',
 	        'payphone', 'heat wave', 'all we know', 'The Neighbourhood - Sweater Weather', 'Sia - Cheap Thrills',
-	        'Ruth B. - Dandelions'  , 'closer', 'on-my-own', 'rockabye', 'starving'  ];
+	        'Ruth B. - Dandelions'  , 'closer', 'on-my-own', 'rockabye', 'starving','just-a-dream'  ];
 
 // Keep track of song
 let songIndex = 0;
